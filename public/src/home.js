ea7eef9627c514e2e1b2f226ab55ae79ff5eca2e
Ryan Bartolome <rahhyahh@icloud.com>
2024-07-08 01:03:14 -1000

    updated the getBooksBorrowedCount();

--- public/src/home.js
+++ public/src/home.js
@@ -7,16 +7,22 @@ function getTotalBooksCount(books) {
 }
 
 function getTotalAccountsCount(accounts) {
-  // YOUR SOLUTION HERE
+  return accounts.length;
 }
 
 function getBooksBorrowedCount(books) {
   // YOUR SOLUTION HERE
   // Hint: You can use the [`filter()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) method here. 
   // If you get stuck, feel free to take a look at this repl.it: https://replit.com/@thinkful/getBooksBorrowedCount#index.js
+
+    return books.filter(book => {
+        const first = book.borrows[0];
+        return first.returned === false;
+    }).length;
 }
 
-// Tbis is a helper function that's called by other functions inside this file. You don't have to edit it.
+
+// This is a helper function that's called by other functions inside this file. You don't have to edit it.
 function _sortObjectByValues(obj) {
   const keys = Object.keys(obj);
   return keys.sort((keyA, keyB) => {
